2aa49838b20b8499ed84db5247be7efca8ab8fd7
coding030 <traderpflanze@gmail.com>
2025-08-09 20:08:24 +0200

    wired up amountHandler and depositHandler

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -12,17 +12,62 @@ import { ethers } from 'ethers'
 
 import Alert from './Alert'
 
-const account = '0x0...'
+import { addLiquidity, loadBalances } from '../store/interactions'
 
 const Deposit = () => {
+	const [token1Amount, setToken1Amount] = useState(0)
+	const [token2Amount, setToken2Amount] = useState(0)
+
+	const provider = useSelector(state => state.provider.connection)
+	const account = useSelector(state => state.provider.account)
+
+	const tokens = useSelector(state => state.tokens.contracts)
+	const symbols = useSelector(state => state.tokens.symbols)
+	const balances = useSelector(state => state.tokens.balances)
+
+	const amm = useSelector(state => state.amm.contract)
+
+	const dispatch = useDispatch()
+
+	const amountHandler = async (e) => {
+		if (e.target.id === 'token1') {
+			setToken1Amount(e.target.value)
+
+			const _token1Amount = ethers.utils.parseUnits(e.target.value, 'ether')
+			const result = await amm.calculateToken2Deposit(_token1Amount)
+			const _token2Amount = ethers.utils.formatUnits(result.toString(), 'ether')
+
+			setToken2Amount(_token2Amount)
+		} else {
+			setToken2Amount(e.target.value)
+
+			const _token2Amount = ethers.utils.parseUnits(e.target.value, 'ether')
+			const result = await amm.calculateToken1Deposit(_token2Amount)
+			const _token1Amount = ethers.utils.formatUnits(result.toString(), 'ether')
+
+			setToken1Amount(_token1Amount)
+		}
+	}
+
+	const depositHandler = async (e) => {
+		e.preventDefault()
+
+		const _token1Amount = ethers.utils.parseUnits(token1Amount, 'ether')
+		const _token2Amount = ethers.utils.parseUnits(token2Amount, 'ether')
+
+		await addLiquidity(provider, amm, tokens, [_token1Amount, _token2Amount], dispatch)
+
+		await loadBalances(amm, tokens, account, dispatch)
+	}
+
 	return (
 		<div>
 			<Card style={{ maxWidth: '450px' }} className='mx-auto px-4'>
 				{account ? (
-					<Form style={{ maxWidth: '450px', margin: '50px auto' }}>
+					<Form onSubmit={depositHandler} style={{ maxWidth: '450px', margin: '50px auto' }}>
 						<Row>
 							<Form.Text className='text-end my-2' muted>
-								Balance:
+								Balance: {balances[0]}
 							</Form.Text>
 							<InputGroup>
 								<Form.Control
@@ -31,13 +76,17 @@ const Deposit = () => {
 									min="0.0"
 									step="any"
 									id="token1"
+									onChange={(e) => amountHandler(e)}
+									value={token1Amount === 0 ? "" : token1Amount}
 								/>
-								<InputGroup.Text style={{ width: "100px" }} className="justify-content-center"></InputGroup.Text>
+								<InputGroup.Text style={{ width: "100px" }} className="justify-content-center">
+									{ symbols && symbols[0] }
+								</InputGroup.Text>
 							</InputGroup>
 						</Row>
 						<Row className='my-3'>
 							<Form.Text className='text-end my-2' muted>
-								Balance:
+								Balance: {balances[1]}
 							</Form.Text>
 							<InputGroup>
 								<Form.Control
@@ -45,8 +94,12 @@ const Deposit = () => {
 									placeholder="0.0"
 									step="any"
 									id="token2"
+									onChange={(e) => amountHandler(e)}
+									value={token2Amount === 0 ? "" : token2Amount}
 								/>
-								<InputGroup.Text style={{ width: "100px" }} className="justify-content-center"></InputGroup.Text>
+								<InputGroup.Text style={{ width: "100px" }} className="justify-content-center">
+									{ symbols && symbols[1] }
+								</InputGroup.Text>
 							</InputGroup>
 						</Row>
 						<Row className='my-3'>
