387ab73defc412370f7167ca26f3922877f36e10
coding030 <traderpflanze@gmail.com>
2025-08-10 13:38:41 +0200

    populated table with event data

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
 import Table from 'react-bootstrap/Table'
+import Chart from 'react-apexcharts'
 import { useEffect } from 'react'
 import { ethers } from 'ethers'
 
@@ -14,6 +15,7 @@ const Charts = () => {
 	const symbols = useSelector(state => state.tokens.symbols)
 
 	const amm = useSelector(state => state.amm.contract)
+	const swaps = useSelector(state => state.amm.swaps)
 
 	const dispatch = useDispatch()
 
@@ -37,15 +39,31 @@ const Charts = () => {
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td>Transaction Hash</td>
-					<td>Token Give</td>
-					<td>Amount Give</td>
-					<td>Token Get</td>
-					<td>Amount Get</td>
-					<td>User</td>
-					<td>Time</td>
-				</tr>
+				{swaps && swaps.map((swap, index) => (
+					<tr key={index}>
+						<td>{swap.hash.slice(0, 5) + '...' + swap.hash.slice(61, 66)}</td>
+						<td>{swap.args.tokenGive === tokens[0].address ? symbols[0] : symbols[1]}</td>
+						<td>{ethers.utils.formatUnits(swap.args.tokenGiveAmount.toString(), 'ether')}</td>
+						<td>{swap.args.tokenGet === tokens[0].address ? symbols[0] : symbols[1]}</td>
+						<td>{ethers.utils.formatUnits(swap.args.tokenGetAmount.toString(), 'ether')}</td>
+						<td>{swap.args.user.slice(0, 5) + '...' + swap.args.user.slice(38, 42)}</td>
+						<td>{
+							new Date(Number(swap.args.timestamp.toString() + '000'))
+								.toLocaleDateString(
+									undefined,
+									{
+										year: 'numeric',
+										month: 'long',
+										day: 'numeric',
+										hour: 'numeric',
+										minute: 'numeric',
+										second: 'numeric'
+									}
+								)
+						}
+						</td>
+					</tr>
+				))}
 			</tbody>
 		</Table>
 	);
